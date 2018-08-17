20242f297442d3f4d46ccf917b303f8417e4a8ae
Eric Olaveson <Eric@Erics-MacBook-Pro.local>
2018-08-17 15:27:10 -0500

    yeah

---
+++ nodbone/server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { json } = require("body-parser");
+const cors = require("cors");
+
+const port = 3001;
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Whistle Tip on port:${port}`);
+});
